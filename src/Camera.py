9ba51283d20882b2a7a41a08827f3b624137940a
Philip Chimento <philip.chimento@gmail.com>
2011-04-27 12:58:52 +0200

    Fix indentation

--- src/Camera.py
+++ src/Camera.py
@@ -1,19 +1,19 @@
 class CameraError(Exception):
-	def __init__(self, msg, cam):
-		self.msg = msg
-		self.camera_number = cam
-	
-	def __str__(self):
-		return '{0} on camera {1}'.format(self.msg, self.camera_number)
+    def __init__(self, msg, cam):
+        self.msg = msg
+        self.camera_number = cam
+
+    def __str__(self):
+        return '{0} on camera {1}'.format(self.msg, self.camera_number)
 
 class Camera(object):
     def __init__(self, cam=-1):
         self.camera_number = cam
         self.frame = None
 
-	def __enter__(self):
-		self.open()
-		return self
+    def __enter__(self):
+        self.open()
+        return self
 
     def __exit__(self, type, value, traceback):
         self.close()
@@ -21,14 +21,13 @@ class Camera(object):
 
     def open(self):
         raise NotImplementedError()
-    
+
     def close(self):
         raise NotImplementedError()
-    
+
     def query_frame(self):
         raise NotImplementedError()
-        
+
     @property
     def resolution(self):
         raise NotImplementedError()
-    
