0a06d14fba93876b3c670f4c2fff782fe8c7f9ae
Philip Chimento <philip.chimento@gmail.com>
2012-04-22 11:49:30 +0200

    Draw ellipse a little faster

--- src/BeamProfiler.py
+++ src/BeamProfiler.py
@@ -65,12 +65,13 @@ class BeamProfiler(HasTraits):
         # Using a parametric equation in t
         t = N.linspace(0, 2 * N.pi, self.num_points)
         angle = N.radians(self._angle)
+        x0, y0 = self._centroid
+        sin_t, cos_t = N.sin(t), N.cos(t)
+        sin_angle, cos_angle = N.sin(angle), N.cos(angle)
         r_a = self._minor_axis / 2.0
         r_b = self._major_axis / 2.0
-        x = (self._centroid[0] + r_a * N.cos(t) * N.cos(angle)
-            - r_b * N.sin(t) * N.sin(angle))
-        y = (self._centroid[1] + r_a * N.cos(t) * N.sin(angle)
-            - r_b * N.sin(t) * N.cos(angle))
+        x = x0 + r_a * cos_t * cos_angle - r_b * sin_t * sin_angle
+        y = y0 + r_a * cos_t * sin_angle - r_b * sin_t * cos_angle
         self.screen.data_store['ellipse_x'] = x
         self.screen.data_store['ellipse_y'] = y
 
