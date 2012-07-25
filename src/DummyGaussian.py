6486b003001683cd9e665f01503d37d20e549c24
Philip Chimento <philip.chimento@gmail.com>
2012-07-24 22:32:47 -0600

    Dynamic boundaries of ranges
    Make the 'centroid' and 'radius' properties of the DummyGaussian plugin
    ranges with dynamic boundaries, so that they can't exceed the
    resolution.

--- src/DummyGaussian.py
+++ src/DummyGaussian.py
@@ -1,16 +1,29 @@
 import time
 import numpy as N
 import numpy.random
-from traits.api import Tuple, Int, Float
-from traitsui.api import View, Item, HGroup, Label
+from traits.api import Int, Constant, Range, Property, cached_property
+from traitsui.api import View, Item, HGroup, VGroup, Label
 
 from Camera import Camera
 
 
 class DummyGaussian(Camera):
 
-    centroid = Tuple(Int(160), Int(120))
-    radius = Float(75.0)
+    # All these proxy properties are necessary because you can only enter the
+    # name of a trait as the dynamic boundary of a range, not an expression.
+    _zero = Constant(0.0)
+    _x_resolution = Property(fget=lambda self: self.resolution[0],
+        depends_on='resolution')
+    _y_resolution = Property(fget=lambda self: self.resolution[1],
+        depends_on='resolution')
+    _half_x_resolution = Property(depends_on='resolution')
+    _half_y_resolution = Property(depends_on='resolution')
+    _half_minimum_resolution = Property(depends_on='resolution')
+    centroid_x = Range('_zero', '_x_resolution', '_half_x_resolution')
+    centroid_y = Range('_zero', '_y_resolution', '_half_y_resolution')
+    # Necessary because Tuple(Range('trait_name', ...), ...) doesn't work
+    centroid = Property(depends_on='centroid_x, centroid_y')
+    radius = Range('_zero', '_half_minimum_resolution', 75)
     amplitude = Int(60000)
     noise_amplitude = Int(5535)
 
@@ -18,7 +31,9 @@ class DummyGaussian(Camera):
         HGroup(
             Item('frame_rate', style='custom'),
             Label('fps')),
-        Item('centroid'),
+        VGroup(
+            Item('centroid_x'),
+            Item('centroid_y')),
         Item('radius'),
         Item('amplitude'),
         Item('noise_amplitude'),
@@ -29,6 +44,25 @@ class DummyGaussian(Camera):
             id_string='Dummy Gaussian Plugin',
             **traits)
         self._supported_resolutions = [(320, 240), (640, 480)]
+
+    @cached_property
+    def _get__half_x_resolution(self):
+        return self.resolution[0] / 2.0
+
+    @cached_property
+    def _get__half_y_resolution(self):
+        return self.resolution[1] / 2.0
+
+    @cached_property
+    def _get__half_minimum_resolution(self):
+        return min(self._half_x_resolution, self._half_y_resolution)
+
+    @cached_property
+    def _get_centroid(self):
+        return (self.centroid_x, self.centroid_y)
+
+    def _set_centroid(self, value):
+        self.centroid_x, self.centroid_y = value
 
     def open(self):
         pass
