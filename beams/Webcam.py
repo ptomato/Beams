e024cba225944cf79238e3c150d5fd53506f6f64
Philip Chimento <philip.chimento@gmail.com>
2016-06-28 00:40:10 -0700

    Port OpenCV plugin to cv2 interface
    Using the cv2 interface instead of the old cv may solve some of the
    problems that people seem to be experiencing using the OpenCV plugin on
    Windows.

--- beams/Webcam.py
+++ beams/Webcam.py
@@ -1,30 +1,9 @@
-import cv
-from cv import CV_CAP_PROP_FRAME_WIDTH as FRAME_WIDTH
-from cv import CV_CAP_PROP_FRAME_HEIGHT as FRAME_HEIGHT
-import numpy as N
+import cv2
+from cv2.cv import CV_CAP_PROP_FRAME_WIDTH as FRAME_WIDTH
+from cv2.cv import CV_CAP_PROP_FRAME_HEIGHT as FRAME_HEIGHT
 from traitsui.api import Item, Label, RangeEditor, VGroup, View
 
 from Camera import Camera, CameraError
-
-
-def ipl2array(im):
-    '''Converts an IplImage @im to a NumPy array.
-    Adapted from http://opencv.willowgarage.com/wiki/PythonInterface'''
-    depth2dtype = {
-        cv.IPL_DEPTH_8U: 'uint8',
-        cv.IPL_DEPTH_8S: 'int8',
-        cv.IPL_DEPTH_16U: 'uint16',
-        cv.IPL_DEPTH_16S: 'int16',
-        cv.IPL_DEPTH_32S: 'int32',
-        cv.IPL_DEPTH_32F: 'float32',
-        cv.IPL_DEPTH_64F: 'float64',
-    }
-
-    a = N.fromstring(im.tostring(),
-                     dtype=depth2dtype[im.depth],
-                     count=im.width * im.height * im.nChannels)
-    a.shape = (im.height, im.width, im.nChannels)
-    return a
 
 
 class Webcam(Camera):
@@ -52,24 +31,20 @@ class Webcam(Camera):
         self._capture = None
 
     def open(self):
-        self._capture = cv.CaptureFromCAM(self.camera_number)
+        self._capture = cv2.VideoCapture(self.camera_number)
 
         # doesn't raise an exception on error, so we test it explicitly
-        iplimage = cv.QueryFrame(self._capture)
-        if iplimage is None:
-            raise CameraError('Could not query image', self.camera_number)
+        if not self._capture.isOpened():
+            raise CameraError('Could not open camera', self.camera_number)
 
     def close(self):
-        try:
-            cv.ReleaseCapture(self._capture)
-        except AttributeError:
-            pass  # ReleaseCapture API was removed in later versions
+        self._capture.release()
 
     def query_frame(self):
-        iplimage = cv.QueryFrame(self._capture)
-        if iplimage is None:
+        success, frame = self._capture.read()
+        if not success:
             raise CameraError('Could not query image', self.camera_number)
-        self.frame = ipl2array(iplimage)
+        self.frame = frame
 
     def _camera_number_changed(self, old, new):
         try:
@@ -81,15 +56,15 @@ class Webcam(Camera):
 
     def _resolution_default(self):
         '''Resolution of the webcam - a 2-tuple'''
-        width = cv.GetCaptureProperty(self._capture, FRAME_WIDTH)
-        height = cv.GetCaptureProperty(self._capture, FRAME_HEIGHT)
+        width = self._capture.get(FRAME_WIDTH)
+        height = self._capture.get(FRAME_HEIGHT)
         return (int(width), int(height))
 
     def _resolution_changed(self, value):
         width, height = value
-        cv.SetCaptureProperty(self._capture, FRAME_WIDTH, width)
-        cv.SetCaptureProperty(self._capture, FRAME_HEIGHT, height)
-        if cv.GetCaptureProperty(self._capture, FRAME_WIDTH) != width:
+        self._capture.set(FRAME_WIDTH, width)
+        self._capture.set(FRAME_HEIGHT, height)
+        if self._capture.get(FRAME_WIDTH) != width:
             raise CameraError('Width {0} not supported'.format(width), self.camera_number)
-        if cv.GetCaptureProperty(self._capture, FRAME_HEIGHT) != height:
+        if self._capture.get(FRAME_HEIGHT) != height:
             raise CameraError('Height {0} not supported'.format(height), self.camera_number)
